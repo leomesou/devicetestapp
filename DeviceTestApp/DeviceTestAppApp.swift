3e082b9a1e03f662b97690e4c15fd0e8dc1a3397
Leandro Sousa <leandro.sousa@avionte.com>
2024-09-21 11:35:36 -0300

    Initial Commit

---
+++ DeviceTestApp/DeviceTestAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceTestAppApp.swift
+//  DeviceTestApp
+//
+//  Created by Leandro Sousa on 2024. 09. 21..
+//
+
+import SwiftUI
+
+@main
+struct DeviceTestAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DeviceTestAppTests/DeviceTestAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  DeviceTestAppTests.swift
+//  DeviceTestAppTests
+//
+//  Created by Leandro Sousa on 2024. 09. 21..
+//
+
+import Testing
+@testable import DeviceTestApp
+
+struct DeviceTestAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
